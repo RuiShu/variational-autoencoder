3fd4c83f83f24665f9c2bb134651cf8dd67e193b
Rui Shu <00ruishu@gmail.com>
2016-04-08 04:41:06 -0700

    Replace manual splitting with torch.split()

--- nnutils/Sampler.lua
+++ nnutils/Sampler.lua
@@ -10,48 +10,26 @@ end
 
 function Sampler:updateOutput(input)
    -- input: batchsize x 2*nMix x nDims
-   local eps = self.eps
-   local output = self.output
-   local len = input:dim()
-   local nMix, mu, lv
-   if len == 3 then
-      nMix = input:size(2)/2
-      mu = input[{{},{1,nMix},{}}]
-      lv = input[{{},{nMix+1,2*nMix},{}}]
-   elseif len == 2 then
-      nMix = input:size(1)/2
-      mu = input[{{1,nMix},{}}]
-      lv = input[{{nMix+1,2*nMix},{}}]
+   self:_viewInput(input)
+   if self.nMix == 1 then
+      self.eps:resizeAs(self.lv):copy(torch.randn(self.lv:size()))
+      self.output:resizeAs(self.lv):copy(self.lv)
+      self.output:div(2):exp():cmul(self.eps):add(self.mu)
+      self.output = self.output:squeeze(self.len-1)
    end
-   if nMix == 1 then
-      eps:resizeAs(lv):copy(torch.randn(lv:size()))
-      output:resizeAs(lv):copy(lv)
-      output:div(2):exp():cmul(eps):add(mu)
-   end
-   self.len = len
-   self.nMix = nMix
-   self.mu = mu
-   self.lv = lv
-   self.output = output:squeeze(len-1)
    return self.output
 end
 
 function Sampler:updateGradInput(input, gradOutput)
-   local gradInput = self.gradInput
-   local eps = self.eps
-   local len = self.len
-   local nMix = self.nMix
-   local mu = self.mu
-   local lv = self.lv
-   gradInput:resizeAs(input)
-   if len == 3 then
-      dMu = gradInput[{{},{1,nMix},{}}]
-      dLv = gradInput[{{},{nMix+1,2*nMix},{}}]
-   elseif len == 2 then
-      dMu = gradInput[{{1,nMix},{}}]
-      dLv = gradInput[{{nMix+1,2*nMix},{}}]
-   end
-   dMu:copy(gradOutput)
-   dLv:copy(lv):div(2):exp():cmul(eps):cmul(gradOutput)
+   self.dMu:copy(gradOutput)
+   self.dLv:copy(self.lv):div(2):exp():cmul(self.eps):cmul(gradOutput)
    return self.gradInput
+end
+
+function Sampler:_viewInput(input)
+   self.len = input:dim()
+   self.nMix = input:size(self.len-1)/2
+   self.mu, self.lv = unpack(input:split(self.nMix, self.len-1))
+   self.gradInput:resizeAs(input)
+   self.dMu, self.dLv = unpack(self.gradInput:split(self.nMix, self.len-1))
 end
