b46b2b8d9972ba06d9d863720bbb1a9b16fc2c41
Rui Shu <00ruishu@gmail.com>
2016-04-15 22:23:54 -0700

    Change freyface recon criterion to gaussian

--- utils/FreyManager.lua
+++ utils/FreyManager.lua
@@ -6,7 +6,7 @@ function FreyManager:__init(batchsize)
    parent.__init(self, batchsize)
    -- get data
    local f = hdf5.open('datasets/freyfaces.hdf5', 'r')
-   self.train = 1-f:read('train'):all():double()
+   self.train = (1-f:read('train'):all():double()):csub(0.5):mul(2)
    f:close()
 end
 
